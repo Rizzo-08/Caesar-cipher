0abb12ba458660591c07ca996c52645ffa74a941
Rizzo-08 <lorenzorizzo246@gmail.com>
2026-06-03 20:53:30 +0200

    primo commit

---
+++ README.md
@@ -0,0 +1,10 @@
+"# Caesar Cipher
+
+A simple Caesar cipher implementation in Python.
+
+## Usage
+Run \`caesar.py\` and follow the prompts to encrypt or decrypt a message.
+
+## Notes
+- Works with alphabet characters only (preserves case)
+- Shift must be between 1 and 25"
---
+++ caesar.py
@@ -0,0 +1,50 @@
+def caesar(text, shift, encrypt=True):
+
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
+    
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+    
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+
+
+while True:    
+    global what_to_do
+    what_to_do = input("Do you want to encrypt or decrypt?\n")    
+    if what_to_do.lower() != "encrypt" and what_to_do.lower() != "decrypt":
+        print("You can choose only between encrypt or decrypt")
+    else:
+        break
+
+text = input("What is the message? --THE CIPHRARY ONLY WORKS WITH APLHABET'S CHARACHTERS--\n")
+str(text)
+
+try:
+    shift_ = input("With which shift?\n")
+    shift_ = int(shift_)
+except:
+    print("Shift must be a number!")
+
+if what_to_do == "encrypt":
+    print(encrypt(text, shift_)) 
+else:
+    print(decrypt(text, shift_))
+
+
+
+    
